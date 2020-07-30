364a3fce45d150c90f5462811dc79ff1c8cad9a5
Anthony Steel <13as261@queensu.ca>
2020-07-29 21:27:52 -0400

    Add missing denominator in ark/asteroid mass ratio

--- scripts/mass.py
+++ scripts/mass.py
@@ -11,7 +11,7 @@ def main():
     m_asteroid_belt = 3.6 * 10 ** 21
     print("Mass of Ark: ", m_ark, " with ", r_outer/km , " km outer radius ",\
             w/km, " km width and ", r_outer-r_inner/km, " km depth.")
-    print("Percentage of m_ark/m_asteroid_belt", m_ark * 100, "%")
+    print("Percentage of m_ark/m_asteroid_belt", m_ark / M_asteroid_belt * 100, "%")
 
 if __name__ == '__main__':
     main()
